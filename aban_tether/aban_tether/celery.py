777499f4f4303fec710f6be3f41fb3685fcbb419
Sarah Saremi <sarasaaremi@gmail.com>
2024-08-24 01:25:25 +0330

    fix celery config

--- aban_tether/aban_tether/celery.py
+++ aban_tether/aban_tether/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 app = Celery('aban_tether')
 
 app.conf.beat_schedule = {
-    'aggregate-and-buy-every-minute': {
+    'AGGREGATE_AND_BUY_FROM_EXCHANGE': {
         'task': 'exchange.tasks.aggregate_and_buy_from_exchange',
         'schedule': crontab(minute='*/1'),  # Runs every minute
     },
